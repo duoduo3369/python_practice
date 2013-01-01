886f07fb15c450698df868806e878dc485684562
杨洋 <duoduo3_69@163.com>
2013-01-01 09:42:49 +0800

    python cookbook 源码

--- duoduo/调试垃圾回收进程.py
+++ duoduo/调试垃圾回收进程.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 *-*
-
+"""
+P319 8.3
+"""
 
 import gc
-def dump_garbagea():
-    pass
+def dump_garbage():
+    print u'\n垃圾:'
+    gc.collect()
+    print u'垃圾对象 :'
+    for g in gc.garbage:
+        s = str(g)
+        if len(s) > 80:
+            s = s[:77] + '_'
+        print type(g),'\n ',s
+
+if __name__ == '__main__':
+    gc.enable()
+    gc.set_debug(gc.DEBUG_LEAK)
+    l = ['abc']
+    l.append(l)
+    del l
+    dump_garbage()
